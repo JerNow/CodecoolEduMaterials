4fd1c3e340add27f541b9444f571d9b6e61c3477
Jerzy Noworolnik <jnoworolnik@gmail.com>
2022-08-08 13:35:30 +0200

    Add AuthDb with migrations, Create Solution Tree

---
+++ API/ServicesConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AuthDb.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+   public static class ServicesConfiguration
+   {
+      public static void AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+      {
+         services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("AuthDb")));
+         services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("EduMaterialsDb")));
+      }
+
+      public static void AddCustomServices(this IServiceCollection services)
+      {
+         
+      }
+   }
+}
---
+++ AuthDb/Context/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthDb.Context
+{
+   public class AuthDbContext : IdentityDbContext
+   {
+      public AuthDbContext(DbContextOptions<AuthDbContext> options)
+         : base(options)
+      {
+      }
+   }
+}
